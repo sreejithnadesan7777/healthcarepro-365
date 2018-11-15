3d90f82419c5632f3b46a6b91a36f32eb2db838b
Sreejith <sreejithnadesan7777@gmail.com>
2018-11-15 08:56:57 +0530

    first commit

---
+++ src/main/java/com/healthcarepro/healthcarepro/HealthcareproApplication.java
@@ -0,0 +1,12 @@
+package com.healthcarepro.healthcarepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareproApplication.class, args);
+	}
+}
